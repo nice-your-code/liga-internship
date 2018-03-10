d31c1b21794013647a47819f4aad9ad0349e26de
Alex <iliechev2003@gmail.com>
2018-03-10 19:42:25 +0300

    job's done

--- javacore-song-task/src/main/java/ru/liga/App.java
+++ javacore-song-task/src/main/java/ru/liga/App.java
@@ -1,7 +1,11 @@
 package ru.liga;
 
 import ru.liga.songtask.content.Content;
-import ru.liga.songtask.domain.SimpleMidiFile;
+import ru.liga.songtask.domain.*;
+
+import java.util.*;
+
+import static java.lang.Math.abs;
 
 /**
  * Всего нот: 15
@@ -28,9 +32,54 @@ import ru.liga.songtask.domain.SimpleMidiFile;
  * 11: 2
  */
 public class App {
+    public static void PutInMap(Map <Object,Integer> m, Object key){
+        if(m.get(key) == null){
+            m.put(key, 1);
+        }else{
+            m.put(key, m.get(key) + 1);
+        }
+    }
+
+    public static void ShowMap(Map <Object, Integer> m){
+        for(Map.Entry<Object, Integer> i: m.entrySet()) {
+            System.out.println(i.getKey() + ": " + i.getValue());
+        }
+    }
+
     public static void main(String[] args) {
         SimpleMidiFile simpleMidiFile = new SimpleMidiFile(Content.ZOMBIE);
-        System.out.println("Количество нот: " + simpleMidiFile.vocalNoteList().size());
+        List<Note> notes = simpleMidiFile.vocalNoteList();
+        System.out.println("Количество нот: " + notes.size());
         System.out.println("Длительность (сек): " + simpleMidiFile.durationMs() / 1000);
+        Map<Object, Integer> high = new TreeMap<>();
+        Map<Object, Integer> longs = new TreeMap<>();
+        Map<Object, Integer> inter = new TreeMap<>();
+        String maxNote = "", minNote = ""; int maxMidi = -2000000000, minMidi = 2000000000;
+        for(int i=0; i < notes.size(); i++){
+            //System.out.println(notes.get(i).sign().fullName() + "  " + notes.get(i).sign().getMidi() + " " + notes.get(i).durationTicks() + " " + notes.get(i).startTick());
+            if(maxMidi < notes.get(i).sign().getMidi()) {
+                maxNote = notes.get(i).sign().fullName();
+                maxMidi = notes.get(i).sign().getMidi();
+            }
+            if(minMidi > notes.get(i).sign().getMidi()) {
+                minNote = notes.get(i).sign().fullName();
+                minMidi = notes.get(i).sign().getMidi();
+            }
+            PutInMap(high, notes.get(i).sign().fullName());
+            PutInMap(longs, notes.get(i).durationTicks() * simpleMidiFile.tickInMs());
+            if(i != 0) {
+                PutInMap(inter, abs(notes.get(i).sign().getMidi() - notes.get(i-1).sign().getMidi()));
+            }
+        }
+        System.out.println("Анализ диапазона:");
+        System.out.println("верхняя: " + maxNote);
+        System.out.println("нижняя: " + minNote);
+        System.out.println("диапазон: " + (maxMidi - minMidi));
+        System.out.println("Анализ длительности нот (мс):" );
+        ShowMap(longs);
+        System.out.println("Анализ нот по высоте:");
+        ShowMap(high);
+        System.out.println("Анализ интервалов:");
+        ShowMap(inter);
     }
 }
